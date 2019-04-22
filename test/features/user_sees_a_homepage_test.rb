5646cc28affe53e0e73e20b4eab16b1352b271fb
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-21 19:55:51 -0600

    Move page tests into their own files

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -2,24 +2,10 @@ require './test/test_helper'
 
 class HomepageTest < CapybaraTestCase
 
-  def test_user_can_see_the_homepage
+  def test_user_sees_a_homepage
     visit '/'
 
     assert page.has_content?("Welcome!")
-    assert_equal 200, page.status_code
-  end
-
-  def test_user_sees_an_error_when_a_page_not_found
-    visit '/blog'
-
-    assert page.has_content?("Page not found.")
-    assert_equal 404, page.status_code
-  end
-
-  def test_user_can_see_about_page
-    visit '/about'
-
-    assert page.has_content?("About Me!")
     assert_equal 200, page.status_code
   end
 
