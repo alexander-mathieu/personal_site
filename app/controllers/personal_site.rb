a1151bef9957dcc4f5633968891a4c6b4a6f6052
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-21 19:39:35 -0600

    Add about page to call method and test for visibility

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -5,17 +5,22 @@ class PersonalSite
   def self.call(env)
     case env["PATH_INFO"]
     when '/' then index
+    when '/about' then about
     else
       error
     end
   end
 
-  def self.error
-    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
-  end
-
   def self.index
     ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.about
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/about.html')]]
+  end
+
+  def self.error
+    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 
 end
